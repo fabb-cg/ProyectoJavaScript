6c005fe43519a7ab61398a76f2b8a5e025db483c
4383Leonardo <leonardomendez2007@cesanrafael.org>
2024-11-19 12:21:49 -0400

    miJavaScript

---
+++ JS/main.js
@@ -0,0 +1,31 @@
+let mensaje= "hola jer";
+
+function sumar(){
+let numero1=4;
+let numero2=5;
+let suma=numero1+numero2
+alert("La suma es"+suma)
+}
+
+function restar(){
+    let numero1=4;
+    let numero2=5;
+    let resta=numero1-numero2
+    alert("La resta es"+resta)
+    }
+
+    function multiplicacion(){
+        let numero1=4;
+        let numero2=5;
+        let multiplicacion=numero1*numero2
+        alert("La multiplicacion es"+multiplicacion)
+    }
+
+    function division(){
+        let numero1=4;
+        let numero2=5;
+        let division=numero1/numero2
+        alert("La division es"+division)
+    }    
+
+
